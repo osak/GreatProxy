b758db613dd5234ad87dd150e1e95c05d575e905
Osamu KOGA(osa_k) <osak.63@gmail.com>
2011-11-27 15:12:36 +0900

    initial commit

---
+++ main.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/env ruby
+
+require 'webrick'
+require 'webrick/httpproxy'
+
+handler = proc { |req, res|
+  puts res.header
+  puts req.unparsed_uri
+  if req.unparsed_uri.match(/^http:\/\/www\.ocw\.titech\.ac\.jp\/index\.php\?.*action=DownLoad/)
+    if res.header['filename'].index("pdf")
+      res.header['content-type'] = "application/pdf"
+    end
+  end
+}
+
+s = WEBrick::HTTPProxyServer.new(:BindAddress => '127.0.0.1', :Port => 8080, :ProxyVia => false, :ProxyContentHandler => handler)
+
+Signal.trap(:SIGINT) do
+  s.shutdown
+end
+
+s.start
